5d5dc408a9ba62cbf73a2e3ee7a55e918642dcba
matiolsz <m.olszanka@be-tse.com>
2023-04-11 15:56:48 +0200

    improve cv proccessing

--- src/main/java/pl/be/cvgeneratorbe/service/PdfParser.java
+++ src/main/java/pl/be/cvgeneratorbe/service/PdfParser.java
@@ -42,7 +42,7 @@ public class PdfParser {
                                               .filter(i -> array[i].contains("Summary"))
                                               .findFirst().orElse(-1);
         int startIndexOfExperience = IntStream.range(0, array.length)
-                                              .filter(i -> array[i].contains("Experience"))
+                                              .filter(i -> array[i].equals("Experience"))
                                               .findFirst().orElse(-1);
         int startIndexOfEducation = IntStream.range(0, array.length)
                                              .filter(i -> array[i].contains("Education"))
@@ -53,50 +53,79 @@ public class PdfParser {
         int indexOfPage1Sign = IntStream.range(0, array.length)
                                         .filter(i -> array[i].contains("page 1"))
                                         .findFirst().orElse(-1);
+        int indexOfPage2Sign = IntStream.range(0, array.length)
+                                        .filter(i -> array[i].contains("page 2"))
+                                        .findFirst().orElse(-1);
+        int indexOfPage3Sign = IntStream.range(0, array.length)
+                                        .filter(i -> array[i].contains("page 3"))
+                                        .findFirst().orElse(-1);
+        int indexOfLastElement = 0;
+        if(indexOfPage1Sign!=-1){
+            indexOfLastElement = indexOfPage1Sign;
+        }
+        if(indexOfPage2Sign!=-1){
+            indexOfLastElement = indexOfPage2Sign;
+        }
+        if(indexOfPage3Sign!=-1){
+            indexOfLastElement = indexOfPage3Sign;
+        }
 
         StringBuilder stringBuilder = new StringBuilder();
-        for (int j = startIndexOfSummary; j < startIndexOfExperience; j++) {
-            stringBuilder.append(array[j+1]);
-        }
-        userCV.setOverallDescription(stringBuilder.toString());
-        stringBuilder.delete(0, stringBuilder.length()-1);
 
-        for (int j = startIndexOfExperience; j < startIndexOfEducation - 3; j = j + 3) {
-            experience.add(new Experience(array[j + 1], array[j + 2], array[j + 3]));
-        }
-        for (int j = startIndexOfEducation; j < startIndexOfSkills - 1; j = j + 3) {
-            if(array[j+1].contains("Licenses & Certifications")){
-                break;
+        if(startIndexOfSummary!=-1) {
+            for (int j = startIndexOfSummary; j < startIndexOfExperience; j++) {
+                stringBuilder.append(array[j + 1]);
             }
-            if(j+2 == startIndexOfSkills) {
-                educationList.add(new Education(array[j + 1], "", ""));
-                break;
+            userCV.setOverallDescription(stringBuilder.toString());
+            if(stringBuilder.length()>0) {
+                stringBuilder.delete(0, stringBuilder.length() - 1);
             }
-            if(j+3 == startIndexOfSkills){
-                educationList.add(new Education(array[j + 1], array[j + 2], ""));
-                break;
-            }
-            educationList.add(new Education(array[j + 1], array[j + 2], array[j + 3]));
-        }
-        for (int j = startIndexOfSkills; j < indexOfPage1Sign - 1; j++) {
-            if(!array[j+1].contains(" • ")){
-                break;
-            }
-            Arrays.stream(array[j + 1].split(" • ")).forEach(i -> skills.add(i.trim()));
         }
 
-        skills.removeIf(s -> s.equals(""));
-        userCV.setDetailedExperienceList(experience);
-        userCV.setEducationList(educationList);
-        if (skills.size() > 1) {
-            for (String skill : skills) {
-                stringBuilder.append(skill.replaceAll("[^A-Za-z0-9]", "") + ", ");
+        if(startIndexOfExperience!=-1) {
+            for (int j = startIndexOfExperience; j < startIndexOfEducation - 3; j = j + 3) {
+                experience.add(new Experience(array[j + 1], array[j + 2], array[j + 3]));
             }
-            stringBuilder.delete(stringBuilder.length() - 2, stringBuilder.length() - 1);
-            userCV.setTechnologyStack(stringBuilder.toString());
+            userCV.setDetailedExperienceList(experience);
         }
 
+        if(startIndexOfEducation!=-1) {
+            for (int j = startIndexOfEducation; j < startIndexOfSkills - 1; j = j + 3) {
+                if (array[j + 1].contains("Licenses & Certifications")) {
+                    break;
+                }
+                if (j + 2 == startIndexOfSkills) {
+                    educationList.add(new Education(array[j + 1], "", ""));
+                    break;
+                }
+                if (j + 3 == startIndexOfSkills) {
+                    educationList.add(new Education(array[j + 1], array[j + 2], ""));
+                    break;
+                }
+                educationList.add(new Education(array[j + 1], array[j + 2], array[j + 3]));
+            }
+            userCV.setEducationList(educationList);
+
+        }
+        if(startIndexOfSkills!=-1) {
+            for (int j = startIndexOfSkills; j < indexOfLastElement - 1; j++) {
+                if (!array[j + 1].contains(" • ")) {
+                    break;
+                }
+                Arrays.stream(array[j + 1].split(" • ")).forEach(i -> skills.add(i.trim()));
+            }
+            skills.removeIf(s -> s.equals(""));
+
+            if (skills.size() > 1) {
+                for (String skill : skills) {
+                    stringBuilder.append(skill.replaceAll("[^A-Za-z0-9]", "") + ", ");
+                }
+                stringBuilder.delete(stringBuilder.length() - 2, stringBuilder.length() - 1);
+                userCV.setTechnologyStack(stringBuilder.toString());
+            }
+        }
         document.close();
         return userCV;
     }
+
 }
--- src/test/java/pl/be/cvgeneratorbe/PdfParserTest.java
+++ src/test/java/pl/be/cvgeneratorbe/PdfParserTest.java
@@ -1,15 +1,55 @@
 package pl.be.cvgeneratorbe;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.web.multipart.MultipartFile;
+import pl.be.cvgeneratorbe.dto.Education;
+import pl.be.cvgeneratorbe.dto.Experience;
+import pl.be.cvgeneratorbe.dto.UserCV;
 import pl.be.cvgeneratorbe.service.PdfParser;
 
 public class PdfParserTest {
-
+//
 //    @Test
 //    public void runMethod() throws IOException {
 //        PdfParser pdfParser = new PdfParser();
-//        pdfParser.parseLinkedInCv();
+//        File file = new File("src/main/resources/cvpm.pdf");
+//        InputStream stream =  new FileInputStream(file);
+//        MultipartFile multipartFileToSend = new MockMultipartFile("file", file.getName(), MediaType.TEXT_HTML_VALUE, stream);
+//        UserCV userCv = pdfParser.parseLinkedInCv(multipartFileToSend);
+//
+//        System.out.println("000000000000");
+//        System.out.println(userCv.getFullName());
+//        System.out.println("000000000000");
+//        System.out.println(userCv.getRole());
+//        System.out.println("000000000000");
+//        if(userCv.getDetailedExperienceList()!=null) {
+//            for (Experience experience : userCv.getDetailedExperienceList()) {
+//                System.out.println(experience.getJobRole());
+//                System.out.println(experience.getTimePeriod());
+//                System.out.println(experience.getDescription());
+//                System.out.println(experience.getCompany());
+//                System.out.println("--------------");
+//            }
+//        }
+//
+//        System.out.println("000000000000");
+//        if(userCv.getEducationList()!=null) {
+//            for (Education education : userCv.getEducationList()) {
+//                System.out.println(education.getSchool());
+//                System.out.println(education.getDescription());
+//                System.out.println(education.getPeriod());
+//                System.out.println("--------------");
+//            }
+//        }
+//
+//
+//        System.out.println(userCv.technologyStack);
 //    }
 }
